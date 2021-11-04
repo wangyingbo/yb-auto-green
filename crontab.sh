60536027302e8ef9b7aecde91a21912551142582
wangyingbo <wangyingbo528@126.com>
2021-11-04 19:33:43 +0800

    add crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# author wangyingbo
+
+echo "--------current path:-------"
+echo $PWD
+cd $PWD
+
+git pull --rebase
+git commit --allow-empty -m "a commit a day keeps your girlfriend away"
+git push
+
+sleep 1
+echo -e "\033[33;40m  ($0) ${TIME}: this shell script execution duration: ${SECONDS}s  \033[0m"
+echo "😊😊😊😊😊😊success excute shell😊😊😊😊😊😊"
+
+
